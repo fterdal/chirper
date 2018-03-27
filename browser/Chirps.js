00e5c1aa0294c356054772cbd5be7551e8c9f127
Finn Terdal <fterdal@gmail.com>
2018-03-27 11:35:07 -0500

    cleanup

--- browser/Chirps.js
+++ browser/Chirps.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import ChirpsList from './ChirpsList';
 import ChirpForm from './ChirpForm';
-import { seedChirps, createChirp } from './utils';
+import { seedChirps, appendChirp } from './utils';
 
 export default class Chirps extends Component {
   constructor(props) {
@@ -15,17 +15,7 @@ export default class Chirps extends Component {
     this.setState({ chirps: seedChirps() })
   }
   addChirp = (newChirpText)  => {
-    // const nextId = this.state.chirps.reduce((highestId, { id }) => {
-    //   return id > highestId ? id : highestId
-    // }, 0) + 1;
-    // this.setState(prevState => {
-    //   return {
-    //     ...this.state,
-    //     chirps: [...prevState.chirps, createChirp(nextId, newChirpText)]
-    //   }
-    // })
-    // TODO: This is terrible. Make it less terrible.
-    this.setState({ chirps: [...this.state.chirps, createChirp(this.state.chirps, newChirpText)]})
+    this.setState({ chirps: appendChirp(this.state.chirps, newChirpText)})
   }
   render() {
     console.log('this.state.chirps', this.state.chirps)
--- browser/utils.js
+++ browser/utils.js
@@ -1,31 +1,3 @@
-const fakeChirpsOLD = [
-  {
-    id: 1,
-    text: 'i just saw a duck-billed goose-heron-swallow',
-    timeStamp: new Date().toString(),
-  },
-  {
-    id: 2,
-    text: 'is that daffy duck??!??!!!',
-    timeStamp: new Date().toString(),
-  },
-  {
-    id: 3,
-    text: 'cluck cluck',
-    timeStamp: new Date().toString(),
-  },
-  {
-    id: 4,
-    text: 'birds are so much cooler than goats, just sayin\'',
-    timeStamp: new Date().toString(),
-  },
-  {
-    id: 5,
-    text: 'sign my petition to outlaw feather dusters',
-    timeStamp: new Date().toString(),
-  },
-];
-
 const fakeChirps = [
   'i just saw a duck-billed goose-heron-swallow',
   'is that daffy duck??!??!!!',
@@ -34,37 +6,21 @@ const fakeChirps = [
   'sign my petition to outlaw feather dusters',
 ]
 
-export const createChirp = (prevChirps, text) => {
+export const appendChirp = (prevChirps, text) => {
   const nextId = prevChirps.reduce((highestId, { id }) => {
     return id > highestId ? id : highestId
   }, 0) + 1;
   return [...prevChirps, {
     id: nextId,
     text,
-    timeStamp: new Date().toLocaleDateString('en-us', {week: 'short'}),
+    timeStamp: new Date().toLocaleDateString('en-us', {
+      hour: 'numeric', minute: 'numeric', second: 'numeric'
+    }),
   }]
 }
 
 export const seedChirps = () => {
   return fakeChirps.reduce((chirpsList, chirp) => {
-    return createChirp(chirpsList, chirp);
+    return appendChirp(chirpsList, chirp);
   }, [])
 };
-
-export const createChirpOLD = (id, text) => ({
-  id,
-  text,
-  // timeStamp: new Date().toString(),
-  timeStamp: new Date().toLocaleDateString('America/Chicago', {week: 'short'}),
-})
-
-export const createChirpOLDOLD = (chirpsList, text) => {
-  const newChirp = {
-    id: chirpsList.reduce((highestId, { id }) => {
-      return id > highestId ? id : highestId
-    }, 0) + 1,
-    text,
-    timeStamp: new Date().toString(),
-  };
-  return [...chirpsList, newChirp]
-}
