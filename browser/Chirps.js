5e3676da61570169d7d8b9ea804710dec5a016be
Finn Terdal <fterdal@gmail.com>
2018-03-27 11:23:53 -0500

    Aha, I think I fixed the seeding bud

--- browser/Chirps.js
+++ browser/Chirps.js
@@ -8,21 +8,27 @@ export default class Chirps extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chirps: seedChirps(),
+      chirps: [],
     }
   }
+  componentWillMount() {
+    this.setState({ chirps: seedChirps() })
+  }
   addChirp = (newChirpText)  => {
-    const nextId = this.state.chirps.reduce((highestId, { id }) => {
-      return id > highestId ? id : highestId
-    }, 0) + 1;
-    this.setState(prevState => {
-      return {
-        ...this.state,
-        chirps: [...prevState.chirps, createChirp(nextId, newChirpText)]
-      }
-    })
+    // const nextId = this.state.chirps.reduce((highestId, { id }) => {
+    //   return id > highestId ? id : highestId
+    // }, 0) + 1;
+    // this.setState(prevState => {
+    //   return {
+    //     ...this.state,
+    //     chirps: [...prevState.chirps, createChirp(nextId, newChirpText)]
+    //   }
+    // })
+    // TODO: This is terrible. Make it less terrible.
+    this.setState({ chirps: [...this.state.chirps, createChirp(this.state.chirps, newChirpText)]})
   }
   render() {
+    console.log('this.state.chirps', this.state.chirps)
     return (
       <div>
         <ChirpForm addChirp={this.addChirp} />
--- browser/utils.js
+++ browser/utils.js
@@ -1,4 +1,4 @@
-const fakeChirps = [
+const fakeChirpsOLD = [
   {
     id: 1,
     text: 'i just saw a duck-billed goose-heron-swallow',
@@ -26,16 +26,39 @@ const fakeChirps = [
   },
 ];
 
-export const seedChirps = () => fakeChirps;
+const fakeChirps = [
+  'i just saw a duck-billed goose-heron-swallow',
+  'is that daffy duck??!??!!!',
+  'cluck cluck',
+  'birds are so much cooler than goats, just sayin\'',
+  'sign my petition to outlaw feather dusters',
+]
 
-export const createChirp = (id, text) => ({
+export const createChirp = (prevChirps, text) => {
+  const nextId = prevChirps.reduce((highestId, { id }) => {
+    return id > highestId ? id : highestId
+  }, 0) + 1;
+  return [...prevChirps, {
+    id: nextId,
+    text,
+    timeStamp: new Date().toLocaleDateString('en-us', {week: 'short'}),
+  }]
+}
+
+export const seedChirps = () => {
+  return fakeChirps.reduce((chirpsList, chirp) => {
+    return createChirp(chirpsList, chirp);
+  }, [])
+};
+
+export const createChirpOLD = (id, text) => ({
   id,
   text,
   // timeStamp: new Date().toString(),
   timeStamp: new Date().toLocaleDateString('America/Chicago', {week: 'short'}),
 })
 
-export const createChirpOLD = (chirpsList, text) => {
+export const createChirpOLDOLD = (chirpsList, text) => {
   const newChirp = {
     id: chirpsList.reduce((highestId, { id }) => {
       return id > highestId ? id : highestId
